2725e346a173f10d5e74cf3ae8039e190769d132
Eric Rollins <rollins.e@husky.neu.edu>
2017-10-26 20:57:07 -0400

    Added comments to interfaces.

--- Phase_B/src/IAdmin.java
+++ Phase_B/src/IAdmin.java
@@ -1,9 +1,39 @@
+/**
+ * An interface describing the functionality of admins in the
+ * plagiarism detection software. Admins are the type of user
+ * that will handle the user data for users of the plagiarism 
+ * detection software.
+ */
 public interface IAdmin extends IUser {
-	void createUser(String _username, String _password, String _name);
-	void updateUser(String _old_username, String _new_username,
-			String _oldPassword, String _newPassword,
-			String _old_name, String _new_name);
-	void deleteUser(String _username);
-	void getUserDetails(String _username);
+	/**
+	 * Creates a user for the application with the specified attributes.
+	 * @param _username - the unique identifier of the created user (used for login)
+	 * @param _password - the created user's password (used to login)
+	 * @param _name - the human name of the created user
+	 * @param _type - the type of the user to be created (Instructor or Admin)
+	 */
+	void createUser(String _username, String _password, String _name, String _type);
+	/**
+	 * Updates the user to have the specified attributes. Any blank field for a 
+	 * new attribute means that that attribute will be left the same.
+	 * @param _old_username - the username of the user to be updated (cannot be blank)
+	 * @param _new_username - the updated username of the specified user
+	 * @param _newPassword - the updated password of the specified user
+	 * @param _new_name - the updated human name of the specified user
+	 */
+	void updateUser(String _old_username, String _new_username, 
+			String _newPassword, String _new_name);
 	
+	/**
+	 * Deletes the specified user from the database
+	 * @param _username - the unique identifier of the user to be deleted.
+	 */
+	void deleteUser(String _username);
+	
+	/**
+	 * Returns a text representation of a user.
+	 * @param _username - the unique identifier of the user to be displayed
+	 * @return a text representation of a user
+	 */
+	String getUserDetails(String _username);
 }
--- Phase_B/src/IInstructor.java
+++ Phase_B/src/IInstructor.java
@@ -1,6 +1,28 @@
+/**
+ * An interface describing the functionality of instructors in the
+ * plagiarism detection software. Instructors are the type of user
+ * that will analyze the output of the plagiarism detection software.
+ */
 public interface IInstructor extends IUser {
+	/**
+	 * Uploads an assignment submission to the database to be considered 
+	 * for plagiarism.
+	 * @param _submission - the assignment to be uploaded
+	 */
 	void uploadAssignmentSubmission(IAssignmentSubmission _submission);
-	IAssignmentTopic analyzeAssignmentTopic();
-	void reviewCode(IAssignmentSubmission _submission);
+	/**
+	 * Sets the assignment topic to be analyzed for plagiarism.
+	 * @param _at_id - the unique identifier of the assignment topic to be analyzed.
+	 */
+	void analyzeAssignmentTopic(String _at_id);
+	/**
+	 * Sets the assignment submission to be reviewed from within the current assignment topic.
+	 * @param _submission- the unique identifier of the assignment submission to be analyzed.
+	 */
+	void reviewCode(IAssignmentSubmission _as_id);
+	/**
+	 * Toggle's the instructor's view of the current assignment submission between original
+	 * and processed.
+	 */
 	void toggleCode();
 }
