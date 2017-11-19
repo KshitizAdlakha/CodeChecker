879b3abb3710b673b0ea1cadcc48d36d9eb83e2a
Eric Rollins <rollins.e@husky.neu.edu>
2017-11-19 16:30:34 -0500

    Added more detail to AssignmentSubmission comparsion.

--- Phase_C/CodeChecker/src/main/java/codechecker/core/services/AssignmentService.java
+++ Phase_C/CodeChecker/src/main/java/codechecker/core/services/AssignmentService.java
@@ -1,5 +1,12 @@
 package codechecker.core.services;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+
 import codechecker.core.entities.Assignment;
 import codechecker.core.services.util.AssignmentList;
 
@@ -18,6 +25,27 @@ public class AssignmentService {
     }
     public double compareAssignmentSubmissions(Long assignmentId, Long otherAssignmentId) {
     	//This needs to be implemented.
+    	FileInputStream in1;
+    	FileInputStream in2;
+		try {
+			in1 = new FileInputStream(new File("src//test//java//codechecker//Test1.java"));
+			in2 = new FileInputStream(new File("src//test//java//codechecker//Test2.java"));
+	        // parse the file
+	        CompilationUnit cu1 = JavaParser.parse(in1);
+	        CompilationUnit cu2 = JavaParser.parse(in2);
+	        /*
+	         * This is where all the visitors will go.
+	         */
+	        
+	        /*
+	         * This is where the diff function will go
+	         * retur diff(cu1.toString(), cu2.toString());
+	         */
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     	return 0;
     }
 }
---
+++ Phase_C/CodeChecker/src/test/java/codechecker/Test1.java
@@ -0,0 +1,11 @@
+package codechecker;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		//This is a test comment
+		String x = "horse";
+
+	}
+
+}
