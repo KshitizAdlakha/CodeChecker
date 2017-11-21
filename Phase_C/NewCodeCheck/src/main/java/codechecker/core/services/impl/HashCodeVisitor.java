b216391158a0b6a370eedc5eba7599b272466422
preethijc <preethi.jc92@hotmail.com>
2017-11-21 15:01:18 -0500

    Update HashCodeVisitor.java

--- Phase_C/NewCodeCheck/src/main/java/codechecker/core/services/impl/HashCodeVisitor.java
+++ Phase_C/NewCodeCheck/src/main/java/codechecker/core/services/impl/HashCodeVisitor.java
@@ -6,13 +6,20 @@ import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 import java.util.HashSet;
 
+//This class visits all the nodes in the AST, obtains their hashcode, and stores those hashcodes into a set.
 public class HashCodeVisitor extends VoidVisitorAdapter<Void> {
     HashSet<Integer> nodeHashCodes = new HashSet<Integer>();
 
+    /*
+     * Getter to return the set containing the hashcodes of all the nodes in the AST
+     */
     public HashSet<Integer> getNodeHashCodes(){
         return this.nodeHashCodes;
     }
 
+    /*
+     * Function to visit the nodes of the AST
+     */
     @Override
     public void visit(CompilationUnit n, Void arg) {
         for(Node child : n.getChildNodes()) {
@@ -20,7 +27,10 @@ public class HashCodeVisitor extends VoidVisitorAdapter<Void> {
             visit(child, arg);
         }
     }
-
+    
+    /*
+     * Function to visit the child nodes of the given node recursively
+     */
     public void visit(Node n, Void arg) {
         for(Node child : n.getChildNodes()) {
             nodeHashCodes.add(child.hashCode());
