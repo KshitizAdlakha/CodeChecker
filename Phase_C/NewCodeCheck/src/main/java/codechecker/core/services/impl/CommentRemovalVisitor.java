8195ff57561adf86fe02403c1eca252a789deaa2
preethijc <BizdizPhichi16>
2017-11-21 22:07:58 -0500

    Added comments to methods involved in  Comment Removal

--- Phase_C/NewCodeCheck/src/main/java/codechecker/core/services/impl/CommentRemovalVisitor.java
+++ Phase_C/NewCodeCheck/src/main/java/codechecker/core/services/impl/CommentRemovalVisitor.java
@@ -4,8 +4,12 @@ import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
+//This class visits all the nodes in the AST and removes the nodes identified as comments
 public class CommentRemovalVisitor extends VoidVisitorAdapter<Void> {
 
+    /*
+     * Function to visit the nodes of the AST and remove the nodes identified as comments
+     */
     @Override
     public void visit(CompilationUnit n, Void arg) {
         n.removeComment();
@@ -14,6 +18,9 @@ public class CommentRemovalVisitor extends VoidVisitorAdapter<Void> {
         }
     }
 
+    /*
+     * Function to visit the child nodes of the given node recursively and remove the nodes identified as comments
+     */
     public void visit(Node n, Void arg) {
         n.removeComment();
         for(Node child : n.getChildNodes()) {
