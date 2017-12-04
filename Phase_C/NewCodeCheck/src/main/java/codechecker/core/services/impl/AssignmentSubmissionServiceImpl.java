74010759ca0a47ee0a6f9a6ee361e4b9af5b63e6
evrollins <evrollins@ccs.neu.edu>
2017-12-03 20:11:45 -0500

    Implemented standarized naming for functions.

--- Phase_C/NewCodeCheck/src/main/java/codechecker/core/services/impl/AssignmentSubmissionServiceImpl.java
+++ Phase_C/NewCodeCheck/src/main/java/codechecker/core/services/impl/AssignmentSubmissionServiceImpl.java
@@ -84,6 +84,8 @@ public class AssignmentSubmissionServiceImpl implements AssignmentSubmissionServ
             CommentRemovalVisitor crv2 = new CommentRemovalVisitor();
             VariableStandardizationVisitor vsv1 = new VariableStandardizationVisitor();
             VariableStandardizationVisitor vsv2 = new VariableStandardizationVisitor();
+            FunctionStandardizationVisitor fsv1 = new FunctionStandardizationVisitor();
+            FunctionStandardizationVisitor fsv2 = new FunctionStandardizationVisitor();
             HashCodeVisitor hcv1 = new HashCodeVisitor();
             HashCodeVisitor hcv2 = new HashCodeVisitor();
 
@@ -91,8 +93,12 @@ public class AssignmentSubmissionServiceImpl implements AssignmentSubmissionServ
             cu2.accept(crv2, null); //All the nodes in the AST generated from the second submission are visited and the nodes identified as comments are removed
             
             //Standardizes variable naming
-//            cu1.accept(vsv1, null);
-//            cu2.accept(vsv2, null);
+            cu1.accept(vsv1, null);
+            cu2.accept(vsv2, null);
+            
+            //Standardizes function naming
+            cu1.accept(fsv1, null);
+            cu2.accept(fsv2, null);
             
             cu1.accept(hcv1, null); //All the nodes in the AST generated from the first submission are visited.
             cu2.accept(hcv2, null); //All the nodes in the AST generated from the second submission are visited.
