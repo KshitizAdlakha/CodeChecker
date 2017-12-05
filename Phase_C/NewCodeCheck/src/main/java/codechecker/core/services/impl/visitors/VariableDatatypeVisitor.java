5b1c52f15e99af594571af8adc0f80e1d7044b0d
Kshitiz Adlakha <adlakhak@ccs.neu.edu>
2017-12-05 01:57:42 -0500

    Assertion Check

--- Phase_C/NewCodeCheck/src/main/java/codechecker/core/services/impl/visitors/VariableDatatypeVisitor.java
+++ Phase_C/NewCodeCheck/src/main/java/codechecker/core/services/impl/visitors/VariableDatatypeVisitor.java
@@ -1,9 +1,15 @@
 package codechecker.core.services.impl.visitors;
 
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.expr.VariableDeclarationExpr;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.List;
 
 public class VariableDatatypeVisitor extends VoidVisitorAdapter<Void> {
@@ -14,7 +20,15 @@ public class VariableDatatypeVisitor extends VoidVisitorAdapter<Void> {
     public void visit(VariableDeclarationExpr n, Void arg) {
         List<VariableDeclarator> myVars = n.getVariables(); // get the variables declared
         for (VariableDeclarator vars: myVars){
-            vars.setType("T"); // Set the datatype of variables to T
+            try{
+
+                vars.setType("T"); // Set the datatype of variables to T
+
+            } catch (AssertionError assertionError){
+                System.out.println(assertionError);
+                System.out.println("\n");
+            }
+
         }
         super.visit(n, arg);
     }
